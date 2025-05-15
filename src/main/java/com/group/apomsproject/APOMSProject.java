e211bd18ab20a5d2a592250cc72ba486ca9541c6
XJ-Ong <oxj0887@gmail.com>
2025-05-15 23:28:56 +0800

    init repo

---
+++ src/main/java/com/group/apomsproject/APOMSProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.group.apomsproject;
+
+/**
+ *
+ * @author prawn
+ */
+public class APOMSProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
