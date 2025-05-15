f5cc05e6993a0c8a76c4d186131d09a44b753b72
XJ-Ong <oxj0887@gmail.com>
2025-05-15 23:44:43 +0800

    test commit
    removed extra comments
    changed print to test world

--- src/main/java/com/group/apomsproject/APOMSProject.java
+++ src/main/java/com/group/apomsproject/APOMSProject.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.group.apomsproject;
 
-/**
- *
- * @author prawn
- */
 public class APOMSProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Test World!");
     }
 }
