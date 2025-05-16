4e6e12e513d58ee32d227b188abf7534d38db24e
zx-0812 <wohaotiancai888@gmail.com>
2025-05-16 13:12:59 +0800

    modify fm

--- src/main/java/com/group/apomsproject/FinanceManager.java
+++ src/main/java/com/group/apomsproject/FinanceManager.java
@@ -3,13 +3,17 @@ package com.group.apomsproject;
 public class FinanceManager extends User{
     
     private int FMID;
-            
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
 
     public FinanceManager(String userName, String userAddress, String userContact) {
         super(userName, userAddress, userContact);
+        this.FMID = FMID;
     }
     
+    public int getFMID() {
+        return FMID;
+    }
+
+    public void setFMID(int FMID) {
+        this.FMID = FMID;
+    }
 }
