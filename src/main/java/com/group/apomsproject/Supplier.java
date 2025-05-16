ce3157efaca562114c68fd849e14b4a40e30a5f2
zx-0812 <wohaotiancai888@gmail.com>
2025-05-16 15:38:37 +0800

    supplier elements

--- src/main/java/com/group/apomsproject/Supplier.java
+++ src/main/java/com/group/apomsproject/Supplier.java
@@ -1,5 +1,38 @@
 package com.group.apomsproject;
 
 public class Supplier {
+    private int supplierID;
+    private String supplierName;
+    private int itemID;
     
+    public Supplier(int supplierID, String supplierName, int itemID)
+    {
+        this.supplierID = supplierID;
+        this.supplierName = supplierName;
+        this.itemID = itemID;
+    }
+
+    public int getSupplierID() {
+        return supplierID;
+    }
+
+    public void setSupplierID(int supplierID) {
+        this.supplierID = supplierID;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public int getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(int itemID) {
+        this.itemID = itemID;
+    }
 }
