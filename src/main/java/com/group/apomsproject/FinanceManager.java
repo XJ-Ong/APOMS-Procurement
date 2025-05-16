69a0a957625581495e7994ae3ed53f009194a257
zx-0812 <wohaotiancai888@gmail.com>
2025-05-16 11:26:48 +0800

    FM folder
    as title

---
+++ src/main/java/com/group/apomsproject/FinanceManager.java
@@ -0,0 +1,15 @@
+package com.group.apomsproject;
+
+public class FinanceManager extends User{
+    
+    private int FMID;
+            
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    public FinanceManager(String userName, String userAddress, String userContact) {
+        super(userName, userAddress, userContact);
+    }
+    
+}
