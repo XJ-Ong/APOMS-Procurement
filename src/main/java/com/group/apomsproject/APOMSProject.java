429a6c79b5b20188a1ba392c0fd5fda4b2969a16
XJ-Ong <oxj0887@gmail.com>
2025-05-19 13:26:56 +0800

    add: ReadFile
    ReadFile reads from a csv file by taking filename imput and returns a Map<> key-value pairs of row data in the form of strings.

--- src/main/java/com/group/apomsproject/APOMSProject.java
+++ src/main/java/com/group/apomsproject/APOMSProject.java
@@ -8,7 +8,7 @@ public class APOMSProject
         InventoryManager IM01 = new InventoryManager("I01", "IMtest", "1234", "PJ", "+6011-11112234");
         FileOperations fh = new FileOperations();
         fh.WriteFile(IM01);
-//        MainGUI main = new MainGUI();
-//        main.setVisible(true);
+        MainGUI main = new MainGUI();
+        main.setVisible(true);
     }
 }
--- src/main/java/com/group/apomsproject/FileOperations.java
+++ src/main/java/com/group/apomsproject/FileOperations.java
@@ -67,4 +67,68 @@ public class FileOperations
             JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
         }
     }
+    
+    public List<Map<String, String>> ReadFile(String filename)
+    {
+        List<Map<String, String>> data = new ArrayList<>();
+        
+        try(BufferedReader br = new BufferedReader(new FileReader(filename)))
+        {
+            String headerLine = br.readLine();
+            
+            if(headerLine == null)
+            {
+                return data;
+            }
+            
+            String[] headers = parseCSVLine(headerLine);
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                String[] values = parseCSVLine(line); // returns a list of String from a csv line
+                Map<String, String> row = new HashMap<>(); 
+                
+                // using Map<> to create key-value pairs for easier data operations
+                for(int i = 0; i < headers.length; i++)
+                {
+                    if(i < values.length)
+                    {
+                    row.put(headers[i], values[i]);
+                    }
+                    else
+                    {
+                        row.put(headers[i], "");
+                    }
+                }
+                data.add(row);
+            }
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "Error: " + e.getMessage());
+        }
+        return data;
+    }
+    
+    private String[] parseCSVLine(String line)
+    {
+        List<String> fields = new ArrayList<>();
+        StringBuilder currentField = new StringBuilder();
+        boolean inQuotes = false;
+
+        for (char c : line.toCharArray()) {
+            if (c == '"') {
+                inQuotes = !inQuotes;
+                continue;
+            }
+            if (c == ',' && !inQuotes) {
+                fields.add(currentField.toString());
+                currentField.setLength(0);
+            } else {
+                currentField.append(c);
+            }
+        }
+        fields.add(currentField.toString());
+        return fields.toArray(new String[0]);
+    }
 }
