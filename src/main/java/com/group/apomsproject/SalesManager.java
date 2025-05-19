78c585a6c575f643d9d403227bea12a0ec430002
zx-0812 <wohaotiancai888@gmail.com>
2025-05-19 15:51:56 +0800

    sales manager

--- src/main/java/com/group/apomsproject/SalesManager.java
+++ src/main/java/com/group/apomsproject/SalesManager.java
@@ -2,4 +2,18 @@ package com.group.apomsproject;
 
 public class SalesManager {
     
+    private String SMID;
+
+    public String getSMID() {
+        return SMID;
+    }
+
+    public void setSMID(String SMID) {
+        this.SMID = SMID;
+    }
+
+    public SalesManager(String SMID) {
+        this.SMID = SMID;
+    }
+
 }
